d9142e42e4cb986a06387971f2448a0c9af9953a
Eric Li <ericli@knights.ucf.edu>
2021-07-08 20:13:43 -0600

    Add arrow functions and beforeEach

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -4,32 +4,41 @@ const expect = chai.expect;
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 
-describe('Deck', function() {
+describe('Deck', () => {
 
-  it('should be a function', function() {
-    const deck = new Deck();
+  let card1, card2, card3, deck;
+
+  beforeEach(() => {
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    deck = new Deck([card1, card2, card3]);
+  });
+
+  it('should be a function', () => {
+    // const deck = new Deck();
 
     expect(Deck).to.be.a('function');
   });
 
-  it('should take in an array of Card objects', function() {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+  it('should take in an array of Card objects', () => {
+    // const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    // const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    // const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
 
-    const deck = new Deck([card1, card2, card3]);
+    // const deck = new Deck([card1, card2, card3]);
 
     expect(deck.cards).to.deep.equal([card1, card2, card3]);
   });
 
-  describe('countCards()', function() {
+  describe('countCards()', () => {
 
-    it('should be able to count the number of cards in the deck', function() {
-      const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-      const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-      const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    it('should be able to count the number of cards in the deck', () => {
+      // const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+      // const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+      // const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
 
-      const deck = new Deck([card1, card2, card3]);
+      // const deck = new Deck([card1, card2, card3]);
 
       const cardQuantity = deck.countCards();
 
